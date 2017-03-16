94f2bfaf32b4a60e7d97468e204f971ff57ae42e
Maxwell Flynn <tinfoilboy@gmail.com>
2017-03-15 23:32:19 -0400

    Added project skeleton
    Added directories for the new code, also started writing the base skeleton for the Node class.

---
+++ CTML/Node/Node.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+namespace CTML
+{
+	/**
+	 * Generic HTML Node class for CTML.
+	 */
+	class Node
+	{
+	public:
+		Node()
+		{
+		}
+
+		~Node()
+		{
+		}
+
+	private:
+		/**
+		 * Determines whether the node is
+		 * essentially a container or "root"
+		 * node.
+		 *
+		 * For example, an HTML document would
+		 * use a empty root node, then add all
+		 * of the children such as DOCTYPE and
+		 * others within.
+		 *
+		 * Defaults to true unless otherwise set.
+		 */
+		bool m_empty = true;
+
+	};
+}
---
+++ CTML/test.cpp
@@ -0,0 +1,15 @@
+/**
+ * A collection of test methods for CTML.
+ *
+ * Cases should be seperate methods that
+ * compares a desired output with the one
+ * returned by the library.
+ *
+ * Once a method is added, add it to the
+ * main method for running a sweep of tests.
+ */
+
+int main()
+{
+	return 0;
+}
