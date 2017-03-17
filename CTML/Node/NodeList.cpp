63fb87cd63345584eead267b4027e5189585ec3e
Maxwell Flynn <tinfoilboy@gmail.com>
2017-03-16 22:23:47 -0400

    Added base methods to the NodeList
    NodeList now contains methods to add and remove. ForEach is also implemented.

---
+++ CTML/Node/NodeList.cpp
@@ -0,0 +1,85 @@
+#include "../stdafx.hpp"
+#include "NodeList.hpp"
+
+#include "Node.hpp"
+
+using namespace CTML;
+
+/**
+Construct a NodeList of the specified size.
+*/
+NodeList::NodeList(const size_t capacity)
+	:
+	m_capacity(capacity)
+{
+	m_nodes = new Node[capacity];
+}
+
+NodeList::~NodeList()
+{
+	delete[] m_nodes;
+
+	m_count = 0;
+	m_capacity = 0;
+}
+
+NodeList& NodeList::Add(const Node& node)
+{
+	// Resize if we are at or over capacity
+	if (m_count >= m_capacity)
+		_ResizeArray();
+
+	m_nodes[m_count] = node;
+
+	m_count++;
+
+	return *this;
+}
+
+NodeList& NodeList::Remove(const size_t item)
+{
+	// Do not try and delete something out of bounds
+	if (item < 0 || item > m_count)
+		return *this;
+
+	// To remove, shift left each item until the end
+	for (size_t index = item; index < m_count; index++)
+		m_nodes[index] = m_nodes[index + 1];
+
+	m_count--;
+
+	_ResizeArray(m_count);
+
+	return *this;
+}
+
+void NodeList::ForEach(const ForEachCallback& callback)
+{
+	if (m_count == 0)
+		return;
+
+	for (size_t index = 0; index < m_count; index++)
+		callback(index, &m_nodes[index]);
+}
+
+void NodeList::_ResizeArray(const ptrdiff_t resizedCapacity)
+{
+	// Calculate the size for the new array
+	size_t capacity = 0;
+
+	if (resizedCapacity < 0)
+		capacity = m_capacity * 2;
+	else
+		capacity = static_cast<size_t>(resizedCapacity);
+
+	// Create a new array of nodes, copy the
+	// current contents over to the new
+	Node* resized = new Node[capacity];
+	memcpy(resized, m_nodes, m_count * sizeof(Node));
+
+	m_capacity = capacity;
+
+	// Clean-up and reset
+	delete[] m_nodes;
+	m_nodes = resized;
+}
--- CTML/Node/NodeList.hpp
+++ CTML/Node/NodeList.hpp
@@ -4,6 +4,8 @@ namespace CTML
 {
 	class Node;
 
+	typedef void(*ForEachCallback)(size_t, Node*);
+
 	/**
 	 A container representing a list of Node instances.
 	 
@@ -14,7 +16,89 @@ namespace CTML
 	class NodeList
 	{
 	public:
+		/**
+		 Construct a NodeList of the specified size.
+		 */
+		NodeList(const size_t capacity=1);
 
+		~NodeList();
+
+		/**
+		 Simply add a new Node to the list.
+
+		 May or may not resize the underlying
+		 array. To avoid an unnecessary resize,
+		 try creating the list with a default
+		 capacity in mind.
+
+		 Since the array might be resized by a
+		 the count multiplied by two, it could
+		 reserve a large amount of memory.
+		 
+		 To mitigate that, resize to the count
+		 when done manipulating the list using
+		 the Capacity method.
+
+		 Returns the NodeList for chainability.
+		 */
+		NodeList& Add(const Node& node);
+
+		/**
+		 Simply remove a Node from the list
+		 using its index in the list.
+
+		 This method does not resize the
+		 underlying array. To unreserve
+		 the memory after removal, resize
+		 to the count of the NodeList.
+
+		 Returns the NodeList for chainability.
+		 */
+		NodeList& Remove(const size_t item);
+
+		/**
+		 Iterates through the whole NodeList, running
+		 the callback method passed in until there are
+		 no more nodes to iterate through.
+		 */
+		void ForEach(const ForEachCallback& callback);
+
+		/**
+		 Grab the amount of Nodes in the NodeList.
+		 */
+		inline size_t Count()
+		{
+			return m_count;
+		}
+
+		/**
+		 Grab the amount of reserved nodes in the
+		 NodeList currently.
+		 */
+		inline size_t Capacity()
+		{
+			return m_capacity;
+		}
+
+		/**
+		 Reserve an array of nodes up to the passed
+		 in capacity value.
+
+		 Do not run this continuously, this copies
+		 memory! Therefore, wait until you are done
+		 manipulating the list, then resize to what
+		 you want.
+
+		 Returns the NodeList for chainability.
+		 */
+		inline NodeList& Capacity(size_t capacity)
+		{
+			this->m_capacity = capacity;
+
+			_ResizeArray(capacity);
+
+			return *this;
+		}
 
 	private:
 		/**
@@ -24,6 +108,9 @@ namespace CTML
 		 is too small, the array is resized
 		 by the current amount of nodes
 		 multiplied by two.
+
+		 This array defaults to a size that will
+		 hold a single node within.
 		 */
 		Node* m_nodes = nullptr;
 
@@ -39,8 +126,22 @@ namespace CTML
 		 
 		 Can be user-sized as to fit and use
 		 less memory in the list.
+
+		 Defaults to one, as the array defaults
+		 to holding one node.
 		 */
 		size_t m_capacity = 0;
+
+		/**
+		 Resize the array for placing elements
+		 within continuously.
+
+		 The resizedCapacity parameter defaults
+		 to negative one, which, any default
+		 number will resize the array by the
+		 capacity multiplied by two.
+		 */
+		void _ResizeArray(const ptrdiff_t resizedCapacity = -1);
 
 	};
 }
